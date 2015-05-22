6eb7c4612f025d8da649b2a7dae0b27089b8d4d8
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-05-22 12:10:53 +0100

    Audio & Intro animations

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroController : MonoBehaviour
+{
+
+	public GameObject nextObject;
+
+	public void ActivateNext ()
+	{
+		nextObject.SetActive (true);
+		gameObject.SetActive (false);
+	}
+
+	public void GoToNextScene ()
+	{
+
+		switch (Application.loadedLevelName) {
+		case "Intro_1":
+			Application.LoadLevel ("Intro_2");
+			break;
+		case "Intro_2":
+			Application.LoadLevel ("Intro_3");
+			break;
+		case "Intro_3":
+			Application.LoadLevel ("Intro_4");
+			break;
+		case "Intro_4":
+			Application.LoadLevel ("Intro_5");
+			break;
+		}
+
+	}
+}
