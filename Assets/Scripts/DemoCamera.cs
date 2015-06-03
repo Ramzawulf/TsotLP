b79e314d9aec01abacd484cc7550a9dc6f2cf167
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-06-03 01:03:13 +0100

    Demo Scene

---
+++ Assets/Scripts/DemoCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoCamera : MonoBehaviour
+{
+	
+	public Transform[] points;
+	public float speed = 0.5f;
+	public float closenessThreshold = 0.5f;
+
+	public Transform finalPoint;
+	public float finalSize = 10;
+
+	public int currentPoint = 0;
+
+	
+	void LateUpdate ()
+	{
+		FollowPoints ();
+	}
+	
+	private void FollowPoints ()
+	{
+		if (points.Length < 1)
+			return;
+		if (currentPoint >= points.Length) {
+			GoToFinalPoint ();
+		} else {
+			Vector3 targetPosition = new Vector3 (points [currentPoint].position.x, points [currentPoint].position.y, transform.position.z);
+			transform.position = Vector3.Lerp (transform.position, targetPosition, speed * Time.deltaTime);
+			if (Vector3.Distance (transform.position, targetPosition) < closenessThreshold) {
+				currentPoint++;
+			}
+		}
+	}
+
+	private void GoToFinalPoint ()
+	{
+		Vector3 targetPosition = new Vector3 (finalPoint.position.x, finalPoint.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, targetPosition, speed * Time.deltaTime);
+
+		Camera cam = GetComponent<Camera> ();
+		cam.orthographicSize = Mathf.Lerp (cam.orthographicSize, finalSize, Time.deltaTime * speed);
+	}
+}
